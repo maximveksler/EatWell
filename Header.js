63647b4a5b66cd4b4cdd3264bba23795805992a6
Maxim Veksler <maxim@vekslers.org>
2016-04-23 21:45:20 +0300

    Attempt to set backgroundImage

--- Header.js
+++ Header.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
+var captionCSS = {
+  position: 'absolute',
+  top: '45%',
+  left: 0,
+  width: '100%'
+}
+
 var Header = React.createClass({
   render: function() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-12">
-            <img src="http://placehold.it/1000x550" alt="test" className="img-responsive" />
-            <div className="carousel-caption">
-              <h1>Eat what you love.</h1>
+      <div className="text-center jumbotron"  style={{ "backgroundImage": "url(assets/img/cover.png)" }}>
+        <div className="container-fluid">
+          <div className="row" >
+            <div className="col-md-12" >
+
+              <div className="caption" style={captionCSS}>
+                <h1>EatWell</h1>
+                <h2>Eat what you love.</h2>
+              </div>
             </div>
           </div>
         </div>
