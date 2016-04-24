fca7e31d98134070fc08914d10c175e337d04c01
Maxim Veksler <maxim@vekslers.org>
2016-04-24 23:02:17 +0300

    Moving style into seperate file

--- Header.js
+++ Header.js
@@ -1,24 +1,15 @@
 import React from 'react'
 
-var captionCSS = {
-  position: 'absolute'
-  ,top: '45%'
-  ,left: 0
-  ,width: '100%'
-}
-
 var backgroundImageCSS = {
-  background: "url(assets/img/cover.png)"
-  ,backgroundSize: "cover"
 }
 
 class Header extends React.Component {
   render() {
     return (
-      <div className="text-center jumbotron"  style={backgroundImageCSS}>
+      <div className="text-center eat-header-caption">
           <div className="row" >
             <div className="col-md-12" >
-              <div className="caption" style={captionCSS}>
+              <div className="caption">
                 <h1>EatWell</h1>
                 <h2>Eat what you love...</h2>
               </div>
